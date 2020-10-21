3a2e32104b938d142f3258390430821d284ad92c
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-10-21 16:10:26 +0200

    Finish

--- script.js
+++ script.js
@@ -2,40 +2,58 @@ var PREZZO_KM = 0.21;
 var SCONTO_GIOVANI = 0.2;
 var SCONTO_ANZIANI = 0.4;
 
-var kmRichiesti = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+var generaBtn = document.getElementById("genera-btn");
+var annullaBtn = document.getElementById("annulla-btn");
 
-var etaPasseggero = parseInt(prompt("Inserisci la tua età"));
+generaBtn.addEventListener("click", function () {
+  // visualizzo il biglietto
 
-var prezzoBiglietto = kmRichiesti * PREZZO_KM;
+  //assegno numero carrozza
+  var randomSeat = Math.floor(Math.random() * 10000 + 90000);
+  var randomCarrozza = Math.floor(Math.random() * 10 + 1);
 
-console.log(prezzoBiglietto);
+  document.getElementById("seat-number").innerHTML = randomSeat;
+  document.getElementById("carrozza-number").innerHTML = randomCarrozza;
 
-if (!isNaN(etaPasseggero) && !isNaN(kmRichiesti)) {
-  if (etaPasseggero < 18) {
-    document.writeln(
-      "Congratulazioni, hai diritto ad uno sconto del 20% il prezzo del tuo biglietto era " +
-        prezzoBiglietto +
-        " Euro" +
-        " adesso è di " +
-        (prezzoBiglietto -= prezzoBiglietto * SCONTO_ANZIANI).toFixed(2) +
-        " Euro"
-    );
-  } else if (etaPasseggero > 65) {
-    document.writeln(
-      "Congratulazioni, hai diritto ad uno sconto del 40% il prezzo del tuo biglietto era " +
-        prezzoBiglietto +
-        " Euro" +
-        " adesso è di " +
-        (prezzoBiglietto -= prezzoBiglietto * SCONTO_GIOVANI).toFixed(2) +
-        " Euro"
-    );
+  //assegno offerta
+
+  var fasciaEta = document.getElementById("fascia-eta").value;
+  document.getElementById("offer").innerHTML = fasciaEta;
+
+  // assegno nome e cognome al biglietto
+
+  var customerName = document.getElementById("name-surname").value;
+
+  document.getElementById("customer-name").innerHTML = customerName;
+
+  // leggo i km da percorrere e li assegno ad una variabile
+
+  var kmRichiesti = document.getElementById("km-richiesti").value;
+
+  // calcolo il costo del biglietto
+
+  var prezzoBiglietto = (kmRichiesti * PREZZO_KM).toFixed(2);
+
+  if (isNaN(kmRichiesti) || customerName == " ") {
+    alert("Dati mancanti o incorretti, riprova.");
+  } else if (fasciaEta == "minorenne") {
+    document.getElementById("biglietto").style.display = "block";
+    var prezzoGiovani = (prezzoBiglietto -=
+      prezzoBiglietto * SCONTO_GIOVANI).toFixed(2);
+    document.getElementById("price").innerHTML = prezzoGiovani + " €";
+  } else if (fasciaEta == "over-65") {
+    document.getElementById("biglietto").style.display = "block";
+    var prezzoAnziani = (prezzoBiglietto -=
+      prezzoBiglietto * SCONTO_ANZIANI).toFixed(2);
+    document.getElementById("price").innerHTML = prezzoAnziani + " €";
   } else {
-    document.writeln(
-      "Non hai diritto a sconti particolari. Il prezzo del tuo biglietto è di " +
-        prezzoBiglietto +
-        " Euro"
-    );
+    document.getElementById("biglietto").style.display = "block";
+    document.getElementById("price").innerHTML = prezzoBiglietto + " €";
   }
-} else {
-  document.writeln("Sicuro di aver messo un numero?");
-}
+});
+
+annullaBtn.addEventListener("click", function () {
+  document.getElementById("biglietto").style.display = "none";
+  document.getElementById("km-richiesti").value = " ";
+  document.getElementById("name-surname").value = " ";
+});
